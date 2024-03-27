a08c82e55ef2be4f7c3fc975ee28c897eab37b51
derrickkimtai <derrickmoio92@gmail.com>
2024-03-27 13:26:45 +0300

    excercise

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""redis exercise"""
+
+import redis
+from uuid import uuid4
+
+class Cache:
+    """Cache class"""
+    def __init__(self):
+        """Constructor"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """Store data in redis"""
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
