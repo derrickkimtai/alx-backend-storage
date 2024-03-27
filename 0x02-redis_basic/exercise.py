22884a30760bd73d4d2d0a96a720dba70f84a9d4
derrickkimtai <derrickmoio92@gmail.com>
2024-03-27 13:48:41 +0300

    excercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float]:
+        """Get data from redis"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data if data else None
+    
+    def get_str(self, key: str) -> str:
+        """Get string data from redis"""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    
+    def get_int(self, key: str) -> int:
+        """Get int data from redis"""
+        return self.get(key, fn=int)
