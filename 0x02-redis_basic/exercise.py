348536cab831019672e2acc45df2ff619c2a168d
derrickkimtai <derrickmoio92@gmail.com>
2024-03-27 14:12:30 +0300

    increment

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,8 +2,20 @@
 """redis exercise"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
+from functools import wraps
 
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count calls to a method"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 class Cache:
     """Cache class"""
@@ -12,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
         key = str(uuid4())
